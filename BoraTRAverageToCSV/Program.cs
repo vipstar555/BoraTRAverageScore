8f6d340fd72a55acfd59ce85f572f16af8a2c7e5
vipstar555 <ygtug89580@yahoo.co.jp>
2020-02-05 15:56:41 +0900

    プロジェクトファイルのアップロードテスト

---
+++ BoraTRAverageToCSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoraTRAverageToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
